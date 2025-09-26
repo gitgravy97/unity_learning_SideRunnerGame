db08a5d3fb7c227e0e622b1305c6bc15786921cf
Gravy Gatsby <mcshaned@umich.edu>
2025-09-26 14:26:54 -0500

    Particle effects ++ soundtrack loop

--- Assets/Course Library/Scripts/PlayerController.cs
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody playerBody;
     public bool gameOver = false;
-
     public float jumpForce = 10;
     public bool isOnGround = true;
-
     public float gravityModifier;
 
+	public ParticleSystem explosionParticle;
+	public ParticleSystem dirtParticle;
+	
+	private Rigidbody playerBody;
 	private Animator playerAnim;
+	
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
@@ -26,7 +28,7 @@ public class PlayerController : MonoBehaviour
         {
             playerBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
-			// playerAnim.SetTrigger("Jump_trig");
+			dirtParticle.Stop();
         }
     }
 
@@ -35,12 +37,17 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+			dirtParticle.Play();
         } else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
             Debug.Log("Game Over");
+			dirtParticle.Stop();
+
 			playerAnim.SetBool("Death_b", true);
 			playerAnim.SetInteger("DeathType_int", 1);
+			
+			explosionParticle.Play();
         }
     }
 }
