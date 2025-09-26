d0a1e1d2cd8e2c309f758853ed310629c38c76b4
Gravy Gatsby <mcshaned@umich.edu>
2025-09-25 20:15:26 -0500

    Man jumps, does not double jump

---
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerBody;
+
+    public float jumpForce = 10;
+    public bool isOnGround = true;
+
+    public float gravityModifier;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+        playerBody = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOnGround && Input.GetKeyDown(KeyCode.Space))
+        {
+            playerBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollision(Collision collision) {
+        isOnGround = true;
+    }
+}
