994b2a26c7fba14ecc04341d130246d4ad57a978
Gravy Gatsby <mcshaned@umich.edu>
2025-09-26 13:51:38 -0500

    death animation, no post-mortem jumps

--- Assets/Course Library/Scripts/PlayerController.cs
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -22,7 +22,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isOnGround && Input.GetKeyDown(KeyCode.Space))
+        if (!gameOver && isOnGround && Input.GetKeyDown(KeyCode.Space))
         {
             playerBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
@@ -39,6 +39,8 @@ public class PlayerController : MonoBehaviour
         {
             gameOver = true;
             Debug.Log("Game Over");
+			playerAnim.SetBool("Death_b", true);
+			playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
